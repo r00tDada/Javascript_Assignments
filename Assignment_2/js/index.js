5f4737796cab277e7a122e2994ae23df9adc4759
r00tDada <sourabh18299@gmail.com>
2022-02-02 18:01:22 +0530

    beautify assignment_1

--- Assignment_2/js/index.js
+++ Assignment_2/js/index.js
@@ -1,20 +1,23 @@
-function createusertable(){
-    var row = prompt("Enter the no. of rows")
-    var col = prompt("Enter the no. of cols")
-    console.log(row+" "+ col);
-
-    for(var i =0;i<parseInt(row);i++){
-        var tr = document.getElementById("mytable").insertRow(i);
-        for(var j=0;j<parseInt(col);j++){
-            var tc = tr.insertCell(j);
-            if(j%2==0){
-                tc.innerHTML = i+1+(j*row);
-            }
-            else{
-                tc.innerHTML = ((j+1)*row)-i;
-            }
-        }
+function createusertable() {
+  var row = prompt("Enter the no. of rows");
+  var col = prompt("Enter the no. of cols");
+  console.log(row + " " + col);
+  function Calculate(R, C) {
+    var ans = row * C;
+    if (C % 2 == 1) {
+      ans += R - row;
+    } else {
+      ans += 1 - R;
     }
+    return ans;
+  }
+  for (var i = 1; i <= parseInt(row); i++) {
+    var table_row = document.getElementById("mytable").insertRow(i - 1);
+    for (var j = 1; j <= parseInt(col); j++) {
+      var table_col = table_row.insertCell(j - 1);
+      table_col.innerHTML = Calculate(i, j);
+    }
+  }
 }
 
-createusertable()
+createusertable();
