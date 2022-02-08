94b6d7643f90909f681a34dd98459a65ff04a763
r00tDada <sourabh18299@gmail.com>
2022-02-09 00:11:38 +0530

    added Assignment_4

--- Assignment_5/index.js
+++ Assignment_5/index.js
@@ -1,8 +1,7 @@
-
-var ROW,COL,table_arr;
 const table = document.getElementById("mytable");
 const btn_list = ["toggle_UD","toggle_LR","shuffle","clearbtn"];
-var click_flg = -1,swap_idx;
+let ROW,COL,table_arr;
+let click_flg = 1,swap_idx;
 
 function checkingInputWithInRange(maxRow,maxCol){
   if(ROW>=0 && ROW<=maxRow && COL>=0 && COL<=maxCol){
@@ -20,11 +19,10 @@ function create_2d_array(){
 
 function create_Table() {
   for (let i = 0; i < ROW; i++) {
-    var table_row = table.insertRow(i);
+    let table_row = table.insertRow(i);
     for (let j = 0; j < COL; j++) {
-      var table_col = table_row.insertCell(j);
+      let table_col = table_row.insertCell(j);
       table_col.addEventListener('click',draggable.bind(this,i,j));
-
     }
   }
 }
@@ -50,7 +48,7 @@ function get_adjacent_Cell_Around_Selected(row,col){
     [1, 0],
     [0, 1],
   ];
-  var canmove = [];
+  let canmove = [];
   for (dir of dxy) {
     if (isSafe(row + dir[0], col + dir[1], row, col)) {
       canmove.push([row+ dir[0], col + dir[1]]);
@@ -93,19 +91,17 @@ function isAdjacentCell(row,col,swap_idx){
   return false;
 }
 
-
-
 function toggle_Up_Down() {
   function Calculate(R, C) {
     return ROW * C + R - ROW;
   }
-  for (var i = 1; i <= ROW; i++) {
-    for (var j = 1; j <= COL; j += 2) {
+  for (let i = 1; i <= ROW; i++) {
+    for (let j = 1; j <= COL; j += 2) {
       table_arr[i - 1][j - 1] = Calculate(i, j);
     }
   }
   for (let i = 1; i <= ROW; i++) {
-    for (var j = 2; j <= COL; j += 2) {
+    for (let j = 2; j <= COL; j += 2) {
       table_arr[ROW - i][j - 1] = Calculate(i, j);
     }
   }
@@ -116,13 +112,13 @@ function toggle_Left_Right() {
   function Calculate(R, C) {
     return COL * R + C - COL;
   }
-  for (var i = 1; i <= ROW; i++) {
+  for (let i = 1; i <= ROW; i++) {
     if (i % 2 == 1) {
-      for (var j = 1; j <= COL; j++) {
+      for (let j = 1; j <= COL; j++) {
         table_arr[i - 1][j - 1] = Calculate(i, j);
       }
     } else {
-      for (var j = 1; j <= COL; j++) {
+      for (let j = 1; j <= COL; j++) {
         table_arr[i - 1][COL - j] = Calculate(i, j);
       }
     }
@@ -176,11 +172,14 @@ function all_btn_toggle(toggle_state){
   }
 }
 
+function reset_Form(){
+  document.getElementById("detail_form").reset();
+}
+
 function clear_content(){
   clear_Table();
   all_btn_toggle(true);
-  document.getElementById("detail_form").reset();
-
+  reset_Form();
 }
 
 function takingInput(){
@@ -190,9 +189,9 @@ function takingInput(){
 
 function get_Colored_given_value(val){
   for(let i=0;i<ROW;i++){
-    var table_row = table.rows[i];
+    let table_row = table.rows[i];
     for(let j=0;j<COL;j++){
-        var table_col = table_row.cells[j];
+        let table_col = table_row.cells[j];
         if(table_arr[i][j]==val){
           table_col.classList.add("color_block");
           table_col.innerHTML = "";
@@ -205,44 +204,45 @@ function submitform(){
   takingInput();
   if(!checkingInputWithInRange(20,20)){
     alert("Please enter the row or column within the range 1 to 20");
+    reset_Form();
     return;
   }
   create_2d_array();
   all_btn_toggle(false);
 }
 
+function swapping_two_blocks(curr_X,curr_Y,dest_X,dest_Y){
+  table_arr[curr_X][curr_Y] =table_arr[dest_X][dest_Y];
+  table_arr[dest_X][dest_Y]= ROW*COL;
+}
+
 function draggable(row,col){
   //  1. check whether the click is on colored or not.
-  if(click_flg==-1){
+  if(click_flg==1){
     if(HasBlankedBlock(row,col,ROW*COL)){
       alert("Colored block is click Pls click valid block")
       return;
     }
-    var curr_colored_idx = get_coordinate_by_value(ROW*COL);
-    var adjacentcell = get_adjacent_Cell_Around_Selected(row,col);
+    let curr_colored_idx = get_coordinate_by_value(ROW*COL);
+    let adjacentcell = get_adjacent_Cell_Around_Selected(row,col);
     coloration_of_Click_Cell(adjacentcell,curr_colored_idx,row,col);
-    swap_idx=[row,col];
+    swap_idx = [row,col];
     click_flg=0;
   }
   else if(click_flg==0){
     if(!HasBlankedBlock(row,col,ROW*COL)){
-      click_flg = -1;
       alert("Selected box is not blank");
-      todo();
-      return;
     }
-    if(isAdjacentCell(row,col,swap_idx)){
-      table_arr[row][col] =table_arr[swap_idx[0]][swap_idx[1]];
-      table_arr[swap_idx[0]][swap_idx[1]]= ROW*COL;
-      click_flg = -1;
-      todo();
+    else {
+      if(isAdjacentCell(row,col,swap_idx)){
+        swapping_two_blocks(row,col,swap_idx[0],swap_idx[1]);
+      }
+      else{
+        alert("Selected box is not adjacent to color box");
+      }
     }
-    else{
-      click_flg = -1;
-      alert("Selected box is not adjacent to color box");
-      todo();
-      return;
-    }
+    todo();
+    click_flg=1;
   }
 
 
--- Practice/js/index.js
+++ Practice/js/index.js
@@ -1,5 +1,18 @@
 
-function x(){
-  var a =9;
-}
+// class User{
+//   constructor(name,age,address){
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
+
+//   display(){
+//     console.log(this.name + " "+this.age+" "+this.address);
+//   }
+// }
+
+// new User("Sourabh","23","Jabalpur").display();
+
+let a =6;
+let a =9;
 console.log(a);
