e9cce8486c96cd8b5204d426cbd9f5e3b0b4b347
r00tDada <sourabh18299@gmail.com>
2022-02-17 19:00:35 +0530

    added forEach

--- Notes/promise_async_await.js
+++ Notes/promise_async_await.js
@@ -1,59 +1,68 @@
 // Ques 1 : Execute Promises sequentially without using promise.all
-const promise_random_array = Array.from({ length: 10 }, () =>
+
+const getPromise = (time) => { 
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Promise resolved for ${time}s`)
+    }, time*1000)
+  })
+}
+
+async function sequentially() {
+  const promise_random_array = Array.from({ length: 10 }, () =>
   Math.floor(Math.random() * 10)
 );
 console.log(promise_random_array);
 
-async function sequentially(promise_random_array) {
-  for (let i in promise_random_array) {
-    let response = await promise(i);
+  console.log('Before For Each Loop')
+  promise_random_array.forEach(async (element,index) => {
+    let response =  await getPromise(element);
     console.log(response);
-  }
-}
-
-sequentially(promise_random_array);
-
-function promise(idx) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        resolve("resolve " + Number(Number(idx) + 1));
-    }, promise_random_array[idx] * 1000);
   });
+  
+  // for (let i = 0; i < promise_random_array.length; i++) {
+  //   const result = await getPromise(promise_random_array[i]);
+  //   console.log(result);
+  // }
+  console.log('After For Each Loop')
 }
+
+sequentially();
+
 
 // // Ques 2 : Execute Promises sequentially untill the rejected once
 
-const promise_random_array = Array.from({ length: 10 }, () =>
-  Math.floor(Math.random() * 10)
-);
-console.log(promise_random_array);
+// const promise_random_array = Array.from({ length: 10 }, () =>
+//   Math.floor(Math.random() * 10)
+// );
+// console.log(promise_random_array);
 
-async function sequentially(promise_random_array) {
-  for (let i in promise_random_array) {
-    try {
-      let response = await promise(i);
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-      return;
-    }
-  }
-}
+// async function sequentially(promise_random_array) {
+//   for (let i in promise_random_array) {
+//     try {
+//       let response = await promise(i);
+//       console.log(response);
+//     } catch (err) {
+//       console.log(err);
+//       return;
+//     }
+//   }
+// }
 
-sequentially(promise_random_array);
+// sequentially(promise_random_array);
 
-function promise(idx) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(() => {
-      let error = false;
-      if (idx == 2) {
-        error = true;
-      }
-      if (!error) {
-        resolve("resolve " + Number(Number(idx) + 1));
-      } else {
-        reject("reject at " + Number(Number(idx) + 1));
-      }
-    }, promise_random_array[idx] * 1000);
-  });
-}
+// function promise(idx) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//       let error = false;
+//       if (idx == 2) {
+//         error = true;
+//       }
+//       if (!error) {
+//         resolve("resolve " + Number(Number(idx) + 1));
+//       } else {
+//         reject("reject at " + Number(Number(idx) + 1));
+//       }
+//     }, promise_random_array[idx] * 1000);
+//   });
+// }
